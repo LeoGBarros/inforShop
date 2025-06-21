c93c393b02f100aeb8dd9fd299f7220d96facf4b
Leonardo Barros <leoguedesbarros@gmail.com>
2025-06-21 12:40:48 -0300

    inicio inventory service

---
+++ Modules/product-service/src/test/java/br/com/leonardo/InforShop/InforShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leonardo.InforShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InforShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
